dadf58ec36a9a045da2ed63f7c25eefca4788e08
ragingrahul <raj17sarma@gmail.com>
2023-06-10 23:51:01 +0530

    first commit

---
+++ app/components/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div className='w-[80%] bg-gray-700'>
+        Hello
+    </div>
+  )
+}
+
+export default Main
---
+++ app/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Image from 'next/image'
+import React, { useState } from 'react';
+import {
+  RuntimeConnector,
+  Extension,
+  WALLET
+} from "@dataverse/runtime-connector";
+
+import Header from './components/Header';
+import Main from './components/Main';
+
+const runtimeConnector:RuntimeConnector=new RuntimeConnector(Extension)
+
+export default function Home() {
+  const [wallet,setWallet]=useState<WALLET>()
+
+  const connectWallet=async()=>{
+    try {
+      const res=await runtimeConnector.connectWallet()
+      setWallet(res.wallet)
+      return res.address
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  return (
+    <main className="flex min-h-screen flex-col items-center h-[120vh]">
+      <Header />
+      <Main />
+      <div className='flex flex-col items-center'>
+        <h1>Hello</h1>
+      </div>
+    </main>
+  )
+}

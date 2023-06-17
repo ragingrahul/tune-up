377ca69c2ed7f531a3e6a9b50b7ef29c99ccc166
ragingrahul <raj17sarma@gmail.com>
2023-06-17 22:48:09 +0530

    createStream

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -7,12 +7,19 @@ import { useStream, useWallet } from "../hooks";
 
 function page() {
   const { connectWallet } = useWallet();
-  const { checkCapability } = useStream();
+  const { checkCapability,loadStreams } = useStream();
 
   const checkingCapability = async () => {
     const res = await checkCapability();
     console.log(res);
   };
+
+  const loadTheStreams=async()=>{
+    const res=await loadStreams({
+      pkh:"did:pkh:eip155:1:0x2160D41c9D711Ca3fA7777211148538eeb431970",
+      modelId:"kjzl6hvfrbw6c7cp6xafsa7ghxh1yfw4bsub1363ehrxhi999vlpxny9k69uoxz"
+    })
+  }
 
   useEffect(() => {
     checkingCapability();
@@ -21,8 +28,11 @@ function page() {
   return (
     <main className="flex min-h-screen flex-col items-center h-[120vh] font-RobotoMono">
       <Header />
-      <button onClick={checkingCapability} className="text-white border">
+      <button onClick={checkingCapability} className="text-white border m-3">
         Connect
+      </button>
+      <button onClick={loadTheStreams} className="text-white border">
+        Load Streams
       </button>
     </main>
   );

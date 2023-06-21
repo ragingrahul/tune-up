caccd857dda002ff16348072541ff1af17466571
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-06-21 15:40:16 +0530

    Feed Page Completed

---
+++ app/components/ProfileCard.tsx
@@ -0,0 +1,50 @@
+"use client";
+import React, { useContext, useEffect, useRef } from "react";
+import Image from "next/image";
+import localFont from "next/font/local";
+import LikeButton from "./LikeButton";
+import VanillaTilt from "vanilla-tilt";
+
+const myFont = localFont({
+  src: "../fonts/Chillax-Bold.ttf",
+  display: "swap",
+});
+
+interface ProfileCardProps {
+  name: string;
+  age: number;
+  image: string;
+}
+
+function ProfileCard(props: ProfileCardProps) {
+  const [liked, setLiked] = React.useState(true);
+
+  return (
+    <div
+      className={
+        "bg-white h-[700px] w-[500px] rounded-[40px] mt-8 relative " +
+        myFont.className
+      }
+    >
+      <Image
+        src={props.image}
+        width={1000}
+        height={100}
+        alt="picture"
+        className="object-cover h-[100%] rounded-[40px]"
+      ></Image>
+      <div className="h-[100%] w-[500px] bg-black absolute top-0 rounded-[40px] overlay"></div>
+      <div className="h-[100%] w-[100%] absolute top-0 flex flex-col justify-between">
+        <div className="flex justify-between p-8">
+          <h1 className="text-white text-[75px]">{props.age}</h1>
+          <LikeButton setLiked={setLiked} liked={liked} />
+        </div>
+        <div>
+          <h1 className="p-8 text-[50px]">{props.name}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
--- app/mainfeed/page.tsx
+++ app/mainfeed/page.tsx
@@ -2,16 +2,28 @@
 import React from "react";
 import Image from "next/image";
 import LikeButton from "../components/LikeButton";
-import VanillaTilt from "vanilla-tilt";
 import localFont from "next/font/local";
+import ProfileCard from "../components/ProfileCard";
 
 const myFont = localFont({
   src: "../fonts/Chillax-Bold.ttf",
   display: "swap",
 });
 
+const profiles = [
+  {
+    name: "Anoy Roy Chowdhury",
+    age: 21,
+    image: "/profile_pic.jpg",
+  },
+  {
+    name: "Rahul Raj Sarma",
+    age: 22,
+    image: "/profile_pic2.jpg",
+  },
+];
+
 function page() {
-  const [liked, setLiked] = React.useState(true);
   return (
     <div
       className={
@@ -19,46 +31,14 @@ function page() {
         myFont.className
       }
     >
-      <div className="flex flex-col items-center w-screen mt-16 h-fit bg-[#FF8080]">
-        <div className="bg-white h-[700px] w-[500px] rounded-[40px] mt-8 relative">
-          <Image
-            src="/profile_pic.jpg"
-            width={1000}
-            height={100}
-            alt="picture"
-            className="object-cover h-[100%] rounded-[40px]"
-          ></Image>
-          <div className="h-[100%] w-[500px] bg-black absolute top-0 rounded-[40px] overlay"></div>
-          <div className="h-[100%] w-[100%] absolute top-0 flex flex-col justify-between">
-            <div className="flex justify-between p-8">
-              <h1 className="text-white text-[75px]">21</h1>
-              <LikeButton setLiked={setLiked} liked={liked} />
-            </div>
-            <div>
-              <h1 className="p-8 text-[50px]">Anoy Roy Chowdhury</h1>
-            </div>
-          </div>
-        </div>
-
-        <div className="bg-white h-[700px] w-[500px] rounded-[40px] mt-8 relative">
-          <Image
-            src="/profile_pic.jpg"
-            width={1000}
-            height={100}
-            alt="picture"
-            className="object-cover h-[100%] rounded-[40px]"
-          ></Image>
-          <div className="h-[100%] w-[500px] bg-black absolute top-0 rounded-[40px] overlay"></div>
-          <div className="h-[100%] w-[100%] absolute  top-0 flex flex-col justify-between">
-            <div className="flex justify-between p-8">
-              <h1 className="text-white text-[75px]">21</h1>
-              <LikeButton setLiked={setLiked} liked={liked} />
-            </div>
-            <div>
-              <h1 className="p-8 text-[50px]">Anoy Roy Chowdhury</h1>
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-col items-center w-screen mt-16 h-fit bg-[#FF8080] pb-32">
+        {profiles.map((profile) => (
+          <ProfileCard
+            name={profile.name}
+            age={profile.age}
+            image={profile.image}
+          />
+        ))}
       </div>
 
       <div className="fixed backdrop-filter top-3 rounded-2xl hover:backdrop-filter-none transition duration-300 ease-in-out hover:bg-[#ff8080] w-[450px] h-[75px] bg-white-900/20 backdrop-blur-[10px] flex justify-center items-center">
@@ -71,7 +51,7 @@ function page() {
         />
       </div>
 
-      <div className="fixed backdrop-filter hover:backdrop-filter-none transition duration-300 ease-in-out hover:bg-white bottom-3 w-[400px] h-[80px] bg-slate-50/20 backdrop-blur-[10px] rounded-3xl flex justify-between items-center">
+      <div className="fixed backdrop-filter hover:backdrop-filter-none transition duration-300 ease-in-out hover:bg-white bottom-3 w-[400px] h-[100px] bg-slate-50/20 backdrop-blur-[10px] rounded-3xl flex justify-between items-center">
         <Image
           src="/ProfileIcon.svg"
           width={60}

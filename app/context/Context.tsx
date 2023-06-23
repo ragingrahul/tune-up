9cc0d41c1bbd8c569c3386f789390131bdee0238
ragingrahul <raj17sarma@gmail.com>
2023-06-24 02:56:33 +0530

    Merge Conflict

--- app/context/Context.tsx
+++ app/context/Context.tsx
@@ -1,13 +1,7 @@
-"use client";
-import React, { FC, ReactNode } from "react";
-import { Extension, RuntimeConnector } from "@dataverse/runtime-connector";
-import {
-  createContext,
-  useEffect,
-  useState,
-  Dispatch,
-  SetStateAction,
-} from "react";
+"use client"
+import React, { FC,ReactNode } from 'react'
+import { Extension,RuntimeConnector } from "@dataverse/runtime-connector";
+import {createContext,useEffect,useState,Dispatch,SetStateAction} from "react"
 
 interface ContextType{
     runtimeConnector:RuntimeConnector|undefined;
@@ -15,29 +9,41 @@ interface ContextType{
     setWalletConnect:Dispatch<SetStateAction<boolean>>;
     pkh: string;
     setPkh:Dispatch<SetStateAction<string>>;
+    ownProfile:StreamRecord;
+    setOwnProfile:Dispatch<SetStateAction<StreamRecord>>;
 }
+
+type StreamRecord=Record<string,
+{app: string;
+modelId: string;
+pkh: string;
+streamContent: {
+    file?: any;
+    content?: any;
+};
+}
+>|undefined
 
 export const DataverseContext=createContext<ContextType>({} as ContextType)
 
-export const DataverseProvider = ({ children }: any) => {
-  const [runtimeConnector, setRuntimeConnector] = useState<RuntimeConnector>();
-  const [walletConnected, setWalletConnect] = useState<boolean>(false);
-  const [pkh, setPkh] = useState<string>("");
+export const DataverseProvider = ({children}:any)=>{
 
     const [runtimeConnector,setRuntimeConnector] =useState<RuntimeConnector>()
     const [walletConnected,setWalletConnect]=useState<boolean>(false)
     const [pkh,setPkh]=useState<string>("")
+    const [ownProfile,setOwnProfile]=useState<StreamRecord>()
 
-  useEffect(() => {
-    createRuntimeConnector();
-  }, []);
+    const createRuntimeConnector =async()=>{
+        const runtimeConnector=new RuntimeConnector(Extension)
+        setRuntimeConnector(runtimeConnector)
+    }
 
     useEffect(()=>{
         createRuntimeConnector()
     },[])
 
     return(
-        <DataverseContext.Provider value={{runtimeConnector,walletConnected,pkh,setWalletConnect,setPkh}}>
+        <DataverseContext.Provider value={{runtimeConnector,walletConnected,pkh,ownProfile,setWalletConnect,setPkh,setOwnProfile}}>
             {children}
         </DataverseContext.Provider>
     )
